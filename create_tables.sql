0bcc9f16520936a419ee7ea8c719d9f7abc39342
Robin G.F. Mamie <robin@mamie.one>
2019-03-17 19:43:55 +0100

    Finish 1st version of table creation

--- create_tables.sql
+++ create_tables.sql
@@ -22,8 +22,7 @@ CREATE TABLE Listing_descr(
     latitude FLOAT,
     longitude FLOAT,
     PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES Listing (id)
-    ON DELETE CASCADE)
+    FOREIGN KEY (id) REFERENCES Listing (id) ON DELETE CASCADE)
 
 CREATE TABLE Listing_details(
     id INTEGER,
@@ -37,8 +36,7 @@ CREATE TABLE Listing_details(
     amenities CHAR(1388),
     square_feet INTEGER,
     PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES Listing (id)
-    ON DELETE CASCADE)
+    FOREIGN KEY (id) REFERENCES Listing (id) ON DELETE CASCADE)
 
 CREATE TABLE Listing_price(
     id INTEGER,
@@ -51,8 +49,7 @@ CREATE TABLE Listing_price(
     guest_included INTEGER,
     extra_people INTEGER,
     PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES Listing (id)
-    ON DELETE CASCADE)
+    FOREIGN KEY (id) REFERENCES Listing (id) ON DELETE CASCADE)
 
 CREATE TABLE Listing_score(
   id INTEGER,
@@ -64,8 +61,18 @@ CREATE TABLE Listing_score(
   review_scores_location INTEGER,
   review_scores_value INTEGER,
   PRIMARY KEY (id),
-  FOREIGN KEY (id) REFERENCES Listing (id)
-  ON DELETE CASCADE)
+  FOREIGN KEY (id) REFERENCES Listing (id) ON DELETE CASCADE)
+  
+CREATE TABLE Listing_cond(
+  id INTEGER,
+  minimum_nights INTEGER,
+  maximum_nights INTEGER,
+  is_business_travel_ready BIT,
+  cancellation_policy CHAR(27),
+  require_guest_profile_picture BIT,
+  require_guest_phone_verification BIT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id) REFERENCES Listing (id) ON DELETE CASCADE)
 
 CREATE TABLE Listing_calender(
   id INTEGER,
@@ -73,26 +80,44 @@ CREATE TABLE Listing_calender(
   available BIT,
   price FLOAT,
   PRIMARY KEY (id),
-  FOREIGN KEY (id) REFERENCES Listing (id)
-  ON DELETE CASCADE)
+  FOREIGN KEY (id) REFERENCES Listing (id) ON DELETE CASCADE)
+  
+CREATE TABLE User(
+  uid INTEGER,
+  uname CHAR(40),
+  PRIMARY KEY (uid))
 
-  CREATE TABLE Review(
-    id INTEGER NOT NULL,
-    uid INTEGER,
-    date DATE,
-    rid INTEGER,
-    comments CHAR(6505),
-    PRIMARY KEY(rid),
-    FOREIGN KEY (uid) REFERENCES User(uid),
-    FOREIGN KEY (id) REFERENCES Listing (id)
-    ON DELETE CASCADE)
+CREATE TABLE Host(
+  uid INTEGER,
+  url CHAR(43),
+  since DATE,
+  about CHAR(13000),
+  response_time CHAR(18),
+  response_rate INTEGER, -- other value for int. percentages? (no decimal point)
+  thumbnail_url CHAR(120),
+  picture_url CHAR(120),
+  nid INTEGER, -- references id not name
+  verifications CHAR(170),
+  PRIMARY KEY (uid),
+  FOREIGN KEY (uid) REFERENCES User (uid) ON DELETE CASCADE,
+  FOREIGN KEY (nid) REFERENCES Neighbourhood)
 
+CREATE TABLE Review(
+  id INTEGER NOT NULL,
+  uid INTEGER,
+  date DATE,
+  rid INTEGER,
+  comments CHAR(6505),
+  PRIMARY KEY(rid),
+  FOREIGN KEY (uid) REFERENCES User(uid),
+  FOREIGN KEY (id) REFERENCES Listing (id) ON DELETE CASCADE)
 
-    CREATE TABLE Neighbourhood(
-      nid INTEGER,
-      -- not in the data provided: to add ourselves somehow.
-      neighbourhood CHAR(31),
-      city CHAR(34),
-      country_code CHAR(2),
-      country CHAR(7),
-      PRIMARY KEY(nid))
+CREATE TABLE Neighbourhood(
+  nid INTEGER,
+  -- not in the data provided: to add ourselves somehow.
+  neighbourhood CHAR(31),
+  city CHAR(34),
+  country_code CHAR(2),
+  country CHAR(7),
+  PRIMARY KEY(nid))
+
