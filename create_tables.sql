08e7fbf5fba12103afecae9c8de9edeedf26e8d0
Robin G.F. Mamie <robin@mamie.one>
2019-03-25 10:56:43 +0100

    Change VARCHAR to VARCHAR2

--- create_tables.sql
+++ create_tables.sql
@@ -1,10 +1,10 @@
 CREATE TABLE Neighbourhood(
   nid INTEGER,
   -- not in the data provided: to add ourselves somehow.
-  neighbourhood VARCHAR(40),
-  city VARCHAR(40),
+  neighbourhood VARCHAR2(40),
+  city VARCHAR2(40),
   country_code CHAR(2),
-  country VARCHAR(7),
+  country VARCHAR2(7),
   PRIMARY KEY(nid)
 );
 
@@ -13,24 +13,24 @@ CREATE TABLE Listing (
     id INTEGER,
     nid INTEGER,
     -- nid: not originally provided
-    listing_url VARCHAR(40),
-    name VARCHAR(150),
-    summary VARCHAR(1000),
-    space VARCHAR(1000),
+    listing_url VARCHAR2(40),
+    name VARCHAR2(150),
+    summary VARCHAR2(1000),
+    space VARCHAR2(1000),
     PRIMARY KEY (id),
     FOREIGN KEY (nid) REFERENCES Neighbourhood (nid)
 );
 
 CREATE TABLE Listing_descr(
     id INTEGER,
-    description VARCHAR(1000),
-    neighborhood_overview VARCHAR(1000),
-    notes VARCHAR(1000),
-    transit VARCHAR(1000),
-    l_access VARCHAR(1000),
-    interaction VARCHAR(1000),
-    house_rules VARCHAR(1000),
-    picture_url VARCHAR(120),
+    description VARCHAR2(1000),
+    neighborhood_overview VARCHAR2(1000),
+    notes VARCHAR2(1000),
+    transit VARCHAR2(1000),
+    l_access VARCHAR2(1000),
+    interaction VARCHAR2(1000),
+    house_rules VARCHAR2(1000),
+    picture_url VARCHAR2(120),
     latitude FLOAT,
     longitude FLOAT,
     PRIMARY KEY (id),
@@ -39,14 +39,14 @@ CREATE TABLE Listing_descr(
 
 CREATE TABLE Listing_details(
     id INTEGER,
-    property_type VARCHAR(22),
-    room_type VARCHAR(15),
+    property_type VARCHAR2(22),
+    room_type VARCHAR2(15),
     accommodates INTEGER,
     bathrooms INTEGER,
     bedrooms INTEGER,
     beds INTEGER,
-    bed_type VARCHAR(13),
-    amenities VARCHAR(1400),
+    bed_type VARCHAR2(13),
+    amenities VARCHAR2(1400),
     square_feet INTEGER,
     PRIMARY KEY (id),
     FOREIGN KEY (id) REFERENCES Listing (id) ON DELETE CASCADE
@@ -83,7 +83,7 @@ CREATE TABLE Listing_cond(
   minimum_nights INTEGER,
   maximum_nights INTEGER,
   is_business_travel_ready CHAR(1),
-  cancellation_policy VARCHAR(27),
+  cancellation_policy VARCHAR2(27),
   require_guest_profile_picture CHAR(1),
   require_guest_phone_verification CHAR(1),
   PRIMARY KEY (id),
@@ -101,21 +101,21 @@ CREATE TABLE Listing_calendar(
   
 CREATE TABLE airbnb_User(
   user_id INTEGER,
-  uname VARCHAR(40),
+  uname VARCHAR2(40),
   PRIMARY KEY (user_id)
 );
 
 CREATE TABLE Host(
   user_id INTEGER,
-  url VARCHAR(43),
+  url VARCHAR2(43),
   since DATE,
   about VARCHAR2(4000),
-  response_time VARCHAR(18),
+  response_time VARCHAR2(18),
   response_rate INTEGER, -- other value for int. percentages? (no decimal point)
-  thumbnail_url VARCHAR(120),
-  picture_url VARCHAR(120),
+  thumbnail_url VARCHAR2(120),
+  picture_url VARCHAR2(120),
   nid INTEGER, -- references id not name
-  verifications VARCHAR(170),
+  verifications VARCHAR2(170),
   PRIMARY KEY (user_id),
   FOREIGN KEY (user_id) REFERENCES airbnb_User (user_id) ON DELETE CASCADE,
   FOREIGN KEY (nid) REFERENCES Neighbourhood
