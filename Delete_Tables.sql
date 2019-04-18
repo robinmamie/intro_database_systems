8a1b4b4be19e7006ef5fc04fc4b84689ffbf6ec7
Eric <eric.jolles@epfl.ch>
2019-04-18 18:04:07 +0200

    Ending parsing

--- Delete_Tables.sql
+++ Delete_Tables.sql
@@ -1,11 +1,16 @@
 drop table host;
-drop table airbnb_user;
-drop table review;
-drop table listing;
-drop table neighbourhood;
+
 drop table listing_calendar;
-drop table listing_cond;
-drop table listing_descr;
-drop table listing_details;
-drop table listing_price;
-drop table listing_score;
+drop table review;
+
+drop table listing;
+drop table airbnb_user;
+drop table host;
+drop table neighbourhood;
+drop table bed_type;
+drop table room_type;
+drop table property_type;
+drop table host_response_time;
+drop table city;
+drop table cancellation_policy;
+
--- create_tables.sql
+++ create_tables.sql
@@ -1,135 +1,162 @@
 CREATE TABLE Neighbourhood(
   nid INTEGER,
-  -- not in the data provided: to add ourselves somehow.
   neighbourhood VARCHAR2(40),
-  city VARCHAR2(40),
-  country_code CHAR(2),
-  country VARCHAR2(7),
   PRIMARY KEY(nid)
 );
 
-
-CREATE TABLE Listing (
-    id INTEGER,
-    nid INTEGER,
-    -- nid: not originally provided
-    listing_url VARCHAR2(40),
-    name VARCHAR2(150),
-    summary VARCHAR2(1000),
-    space VARCHAR2(1000),
-    PRIMARY KEY (id),
-    FOREIGN KEY (nid) REFERENCES Neighbourhood (nid)
+CREATE TABLE City(
+  cid INTEGER,
+  city VARCHAR2(40),
+  country VARCHAR2(7),
+  country_code CHAR(2),
+  PRIMARY KEY(cid)
 );
 
-CREATE TABLE Listing_descr(
-    id INTEGER,
-    description VARCHAR2(1000),
-    neighborhood_overview VARCHAR2(1000),
-    notes VARCHAR2(1000),
-    transit VARCHAR2(1000),
-    l_access VARCHAR2(1000),
-    interaction VARCHAR2(1000),
-    house_rules VARCHAR2(1000),
-    picture_url VARCHAR2(120),
-    latitude FLOAT,
-    longitude FLOAT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES Listing (id) ON DELETE CASCADE
+--CREATE TABLE Airbnb_user(
+--  user_id INTEGER,
+--  user_name VARCHAR2(40),
+--  PRIMARY KEY (user_id)
+--);
+
+CREATE TABLE Bed_type(
+  btid INTEGER,
+  bed_type VARCHAR2(13),
+  PRIMARY KEY (btid)
 );
 
-CREATE TABLE Listing_details(
-    id INTEGER,
-    property_type VARCHAR2(22),
-    room_type VARCHAR2(15),
-    accommodates INTEGER,
-    bathrooms INTEGER,
-    bedrooms INTEGER,
-    beds INTEGER,
-    bed_type VARCHAR2(13),
-    amenities VARCHAR2(1400),
-    square_feet INTEGER,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES Listing (id) ON DELETE CASCADE
-);
-
-CREATE TABLE Listing_price(
-    id INTEGER,
-    price INTEGER,
-    weekly_price INTEGER,
-    monthly_price INTEGER,
-    security_deposit INTEGER,
-    cleaning_fee INTEGER,
-    guest_included INTEGER,
-    extra_people INTEGER,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES Listing (id) ON DELETE CASCADE
-);
-
-CREATE TABLE Listing_score(
-  id INTEGER,
-  review_scores_rating INTEGER,
-  review_scores_accuracy INTEGER,
-  review_scores_cleanliness INTEGER,
-  review_scores_checkin INTEGER,
-  review_scores_communication INTEGER,
-  review_scores_location INTEGER,
-  review_scores_value INTEGER,
-  PRIMARY KEY (id),
-  FOREIGN KEY (id) REFERENCES Listing (id) ON DELETE CASCADE
-);
-  
-CREATE TABLE Listing_cond(
-  id INTEGER,
-  minimum_nights INTEGER,
-  maximum_nights INTEGER,
-  is_business_travel_ready CHAR(1),
+CREATE TABLE Cancellation_policy(
+  cpid INTEGER,
   cancellation_policy VARCHAR2(27),
-  require_guest_profile_picture CHAR(1),
-  require_guest_phone_verification CHAR(1),
-  PRIMARY KEY (id),
-  FOREIGN KEY (id) REFERENCES Listing (id) ON DELETE CASCADE
+  PRIMARY KEY (cpid)
 );
 
-CREATE TABLE Listing_calendar(
-  id INTEGER,
-  cdate DATE,
-  available CHAR(1),
-  price FLOAT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (id) REFERENCES Listing (id) ON DELETE CASCADE
+CREATE TABLE Host_response_time(
+  hrtid INTEGER,
+  host_response_time VARCHAR2(18),
+  PRIMARY KEY (hrtid)
 );
-  
-CREATE TABLE airbnb_User(
-  user_id INTEGER,
-  uname VARCHAR2(40),
-  PRIMARY KEY (user_id)
+
+CREATE TABLE Property_type(
+  ptid INTEGER,
+  property_type VARCHAR2(22),
+  PRIMARY KEY (ptid)
+);
+
+CREATE TABLE Room_type(
+  rtid INTEGER,
+  room_type VARCHAR2(15),
+  PRIMARY KEY (rtid)
 );
 
 CREATE TABLE Host(
   user_id INTEGER,
+  user_name VARCHAR2(40),
   url VARCHAR2(43),
   since DATE,
   about VARCHAR2(4000),
-  response_time VARCHAR2(18),
+  response_time INTEGER,
   response_rate INTEGER, -- other value for int. percentages? (no decimal point)
   thumbnail_url VARCHAR2(120),
   picture_url VARCHAR2(120),
   nid INTEGER, -- references id not name
   verifications VARCHAR2(170),
   PRIMARY KEY (user_id),
-  FOREIGN KEY (user_id) REFERENCES airbnb_User (user_id) ON DELETE CASCADE,
+  FOREIGN KEY (response_time) REFERENCES Host_response_time(hrtid),
+ -- FOREIGN KEY (user_id) REFERENCES airbnb_User (user_id) ON DELETE CASCADE,
   FOREIGN KEY (nid) REFERENCES Neighbourhood
 );
 
+
+CREATE TABLE Listing (
+    id INTEGER,
+    listing_url VARCHAR2(40),
+    name VARCHAR2(150),
+    summary VARCHAR2(1500),
+    space VARCHAR2(1500),
+    description VARCHAR2(1500),
+    neighborhood_overview VARCHAR2(1500),
+    notes VARCHAR2(1500),
+    transit VARCHAR2(1500),
+    l_access VARCHAR2(1500),
+    interaction VARCHAR2(1500),
+    house_rules VARCHAR2(1500),
+    picture_url VARCHAR2(120),
+    user_id INTEGER,
+    nid INTEGER,
+    cid INTEGER,
+    latitude FLOAT,
+    longitude FLOAT,
+    ptid INTEGER,
+    rtid INTEGER,
+    accommodates INTEGER,
+    bathrooms FLOAT,
+    bedrooms INTEGER,
+    beds INTEGER,
+    btid INTEGER,
+    amenities VARCHAR2(1400),
+    square_feet INTEGER,
+    price FLOAT,
+    weekly_price FLOAT,
+    monthly_price FLOAT,
+    security_deposit FLOAT,
+    cleaning_fee FLOAT,
+    guests_included INTEGER,
+    extra_people FLOAT,
+    minimum_nights INTEGER,
+    maximum_nights INTEGER,
+    review_scores_rating INTEGER,
+    review_scores_accuracy INTEGER,
+    review_scores_cleanliness INTEGER,
+    review_scores_checkin INTEGER,
+    review_scores_communication INTEGER,
+    review_scores_location INTEGER,
+    review_scores_value INTEGER,
+    is_business_travel_ready CHAR(1),
+    cpid INTEGER,
+    require_guest_profile_picture CHAR(1),
+    require_guest_phone_verification CHAR(1),
+    
+    
+    PRIMARY KEY (id),
+    FOREIGN KEY (cid) REFERENCES City (cid),
+    FOREIGN KEY (user_id) REFERENCES Host(user_id),
+    FOREIGN KEY (ptid) REFERENCES Property_type (ptid),
+    FOREIGN KEY (rtid) REFERENCES Room_type (rtid),
+    FOREIGN KEY (btid) REFERENCES Bed_type (btid),
+    FOREIGN KEY (cpid) REFERENCES Cancellation_policy (cpid),
+    FOREIGN KEY (nid) REFERENCES Neighbourhood (nid)
+);
+
+
+
 CREATE TABLE Review(
-  id INTEGER NOT NULL,
-  user_id INTEGER,
-  rdate DATE,
   rid INTEGER,
+  listing_id INTEGER NOT NULL,
+  user_id INTEGER,
+  user_name VARCHAR2(60),
+  rdate DATE,
   comments VARCHAR2(4000),
   PRIMARY KEY(rid),
-  FOREIGN KEY (user_id) REFERENCES Airbnb_user(user_id),
-  FOREIGN KEY (id) REFERENCES Listing (id) ON DELETE CASCADE
+  --FOREIGN KEY (user_id) REFERENCES Host(user_id),
+  FOREIGN KEY (listing_id) REFERENCES Listing (id) ON DELETE CASCADE
 );
+
+
+
+CREATE TABLE Listing_calendar(
+  listing_id INTEGER,
+  cdate DATE,
+  available CHAR(1),
+  price FLOAT,
+  --PRIMARY KEY (listing_id),
+  FOREIGN KEY (listing_id) REFERENCES Listing (id) ON DELETE CASCADE
+);
+
+
+  
+
+
+
+
 
 
