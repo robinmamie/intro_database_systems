f733fc2d3a4f905f2b04ecf9d008c4c4cb474a83
ejolles <jolleseric@gmail.com>
2019-05-02 15:19:08 +0200

    Adding querry 8

--- sql/DB_Group11-Milestone_3.sql
+++ sql/DB_Group11-Milestone_3.sql
@@ -1,4 +1,4 @@
-
+--9
 
 --1
 SELECT C.city, cnt
@@ -155,3 +155,26 @@ FROM
     ) Cerise
   )
 WHERE row_number <=3;
+
+--8
+CREATE VIEW amenity_list_c AS
+SELECT listing_id, COUNT(*) AS cnt FROM Has_amenity GROUP BY listing_id ;
+SELECT L1.REVIEW_SCORES_COMMUNICATION - L2.REVIEW_SCORES_COMMUNICATION
+FROM Listing L1,
+  Listing L2
+WHERE L1.id IN
+  (SELECT alc1.listing_id
+  FROM amenity_list_c alc1
+  WHERE alc1.cnt =
+    (SELECT MAX (alc2.cnt) FROM amenity_list_c alc2
+    )
+  FETCH FIRST 1 ROWS ONLY
+  )
+AND L2.id IN
+  (SELECT alc1.listing_id
+  FROM amenity_list_c alc1
+  WHERE alc1.cnt =
+    (SELECT MIN (alc2.cnt) FROM amenity_list_c alc2
+    )
+  FETCH FIRST 1 ROWS ONLY
+  ) ;
