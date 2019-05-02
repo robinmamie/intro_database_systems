94219412e65914c74f58eb2d26ee7427b64bbc9c
ejolles <jolleseric@gmail.com>
2019-05-02 12:39:22 +0200

    Adding querry 6

--- sql/DB_Group11-Milestone_3.sql
+++ sql/DB_Group11-Milestone_3.sql
@@ -1,13 +1,3 @@
---6
-SELECT cnt, row_number() over ( partition BY L.host_id order by cnt DESC) row_number
-FROM Listing L,
-  (SELECT listing_id AS lid, COUNT(*) AS cnt FROM Review GROUP BY listing_id
-  )
-WHERE review_scores_rating IS NOT NULL AND L.id = lid;
-  
-Select listing_id as lid, Count(*) as cnt 
-From Review
-Group by listing_id;
 
 
 --1
@@ -122,3 +112,20 @@ FROM
     )
   )
 WHERE row_number <= 3;
+
+
+--6
+SELECT *
+FROM
+  (SELECT L.host_id,
+    lid,
+    cnt,
+    row_number() over ( partition BY L.host_id order by cnt DESC) row_number
+  FROM Listing L,
+    (SELECT listing_id AS lid, COUNT(*) AS cnt FROM Review GROUP BY listing_id
+    )
+  WHERE review_scores_rating IS NOT NULL
+  AND L.id                    = lid
+  )
+WHERE row_number <=3;
+SELECT listing_id AS lid, COUNT(*) AS cnt FROM Review GROUP BY listing_id;
