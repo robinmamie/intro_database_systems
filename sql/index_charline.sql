f68a227a63d0459099078c1c590448ac2e719e31
Charline Montial <charline.montial@epfl.ch>
2019-05-27 13:02:07 +0200

    speed up 10th query

--- sql/index_charline.sql
+++ sql/index_charline.sql
@@ -1,20 +1,15 @@
--- index to speed up 9th query
-CREATE INDEX REVIEW_LID_IDX
-ON REVIEW (LISTING_ID);
+-- speed up 10th query
+CREATE INDEX Listing_nid
+ON Listing (nid);
 
-DROP INDEX REVIEW_LID_IDX;
+DROP INDEX Listing_nid;
 
--- index to speed up 10th query
-CREATE INDEX Calendar_idx_lst
-ON Listing_Calendar (listing_id);
-
-DROP INDEX Calendar_idx_1st;
 
 -- Eric's old stuff
 CREATE INDEX Calendar_idx
 ON Listing_Calendar (extract(YEAR FROM cdate), available);
 
-DROP INDEX Calendar_idx;
+DROP INDEX Calendar_idx_2;
 
 CREATE INDEX Calendar_idx_2
 ON Listing_Calendar (extract(YEAR FROM cdate));
