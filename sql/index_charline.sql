1b4d04a62242bcb9c377362a82931f46768e26c1
Charline Montial <charline.montial@epfl.ch>
2019-05-29 23:46:28 +0200

    Accelerate 3 queries

--- sql/index_charline.sql
+++ sql/index_charline.sql
@@ -1,15 +1,26 @@
--- speed up 10th query
+-- speed up 10th & 11th query
 CREATE INDEX Listing_nid
 ON Listing (nid);
 
 DROP INDEX Listing_nid;
 
-
--- Eric's old stuff
+-- speed up 10th & 11th query
 CREATE INDEX Calendar_idx
 ON Listing_Calendar (extract(YEAR FROM cdate), available);
 
-DROP INDEX Calendar_idx_2;
+DROP INDEX Calendar_idx;
+
+--
+
+CREATE INDEX ham
+ON Listing (rtid);
+
+DROP INDEX ham;
+
+CREATE INDEX ham2
+ON review (listing_id);
+
+DROP INDEX ham2;
 
 CREATE INDEX Calendar_idx_2
 ON Listing_Calendar (extract(YEAR FROM cdate));
