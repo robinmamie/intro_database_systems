bde6d628e597a29bf9ddf897d30056e3595f5195
Robin G.F. Mamie <robin@mamie.one>
2019-06-04 23:44:50 +0200

    Fix bug

--- interface/src/main/scala/database/Lookup.scala
+++ interface/src/main/scala/database/Lookup.scala
@@ -11,7 +11,11 @@ object Lookup {
   def allAttributes(toSearch: String, tables: List[String]): List[Result] = {
     Parameters.tables filter {case (t, _) => tables contains t } map { case(t, a) =>
       val sb = new StringBuilder
-      sb ++= s"SELECT ${Parameters.getPrimaryAttributes(t).head} FROM ${t} WHERE"
+      val pa = Parameters.getPrimaryAttributes(t)
+      if (pa == Nil)
+        sb ++= s"SELECT ${a.head} FROM ${t} WHERE"
+      else
+        sb ++= s"SELECT ${pa.head} FROM ${t} WHERE"
       for (i <- a) sb ++= s" ${i} LIKE '%${toSearch}%' OR"
       DatabaseLink.fetch(sb.toString.dropRight(3), true, -1)
     }
